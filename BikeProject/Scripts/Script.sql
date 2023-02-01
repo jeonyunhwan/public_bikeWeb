9a4e7c577ab6eca72482364b54e447bc1ccfff28
youjean1101 <114139279+youjean1101@users.noreply.github.com>
2023-02-01 21:57:55 +0900

    0201 깃 연동테스트(svn적용전)

---
+++ BikeProject/Scripts/Script.sql
@@ -0,0 +1,13 @@
+SELECT * FROM user1;
+
+SELECT * FROM MEMBER;
+
+CREATE TABLE MEMBER(
+	id varchar(50),
+	pass varchar(50),
+	name varchar(50),
+	auth varchar(50),
+	point number
+);
+
+INSERT INTO MEMBER VALUES('himan','1234','홍길동','관리자',300);
